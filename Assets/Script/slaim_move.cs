75a0c89097943d26b7991b3b0989c85e9448ef9f
Wuppu <wndud1640@naver.com>
2019-10-01 16:14:51 +0900

    Tile map
    타일맵 추가 및 수정 필요

--- Assets/Script/slaim_move.cs
+++ Assets/Script/slaim_move.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class slaim_move : MonoBehaviour {
     public float jumpPower = 3;
 
+    private bool isJump;
+    private float move;
     private Animator an;
     private Vector3 movement;
     private SpriteRenderer sp;
@@ -19,23 +21,36 @@ public class slaim_move : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         movement = Vector3.zero;
+        
+        isJump = false;
         AnimationUpdate(false);
-        if (Input.GetAxisRaw("Horizontal") < 0) {
+        if (Input.GetAxisRaw("Horizontal") < 0 || move < 0) {
             movement = Vector3.left;
             AnimationUpdate(true);
             if (sp.flipX == true) sp.flipX = false;
         }
-        else if (Input.GetAxisRaw("Horizontal") > 0) {
+        else if (Input.GetAxisRaw("Horizontal") > 0 || move > 0) {
             movement = Vector3.right;
             AnimationUpdate(true);
             if (sp.flipX == false) sp.flipX = true;
         }
-        if (Input.GetAxisRaw("Vertical") > 0 && rig.velocity.y < 0.01 && rig.velocity.y > -0.01) {
+        if ((Input.GetAxisRaw("Vertical") > 0 || isJump == true) && rig.velocity.y < 0.01 && rig.velocity.y > -0.01) {
             rig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
         this.transform.position += movement * 1f * Time.deltaTime;
+        move = 0;
     }
     void AnimationUpdate(bool b) {
         an.SetBool("isMoving", b);
+    }
+
+    public void MoveRight() {
+        move = 1f;
+    }
+    public void MoveLeft() {
+        move = -1f;
+    }
+    public void Jump() {
+        isJump = true;
     }
 }
