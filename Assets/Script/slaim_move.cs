445ca600d4aff8dc70f663aac5e21822c369b445
Wuppu <wndud1640@naver.com>
2019-10-01 12:25:51 +0900

    sprite 추가 및 수정, 카메라 이동 구현
    구름 추가, 메뉴 패널 이미지 추가, 카메라 이동 구현

--- Assets/Script/slaim_move.cs
+++ Assets/Script/slaim_move.cs
@@ -2,21 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class slaim_move : MonoBehaviour
-{
+public class slaim_move : MonoBehaviour {
+    public float jumpPower = 3;
+
     private Animator an;
     private Vector3 movement;
     private SpriteRenderer sp;
+    private Rigidbody2D rig;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         an = this.GetComponent<Animator>();
         sp = this.GetComponent<SpriteRenderer>();
+        rig = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         movement = Vector3.zero;
         AnimationUpdate(false);
         if (Input.GetAxisRaw("Horizontal") < 0) {
@@ -24,12 +25,14 @@ public class slaim_move : MonoBehaviour
             AnimationUpdate(true);
             if (sp.flipX == true) sp.flipX = false;
         }
-         else if (Input.GetAxisRaw("Horizontal") > 0) {
+        else if (Input.GetAxisRaw("Horizontal") > 0) {
             movement = Vector3.right;
             AnimationUpdate(true);
             if (sp.flipX == false) sp.flipX = true;
         }
-        
+        if (Input.GetAxisRaw("Vertical") > 0 && rig.velocity.y < 0.01 && rig.velocity.y > -0.01) {
+            rig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
         this.transform.position += movement * 1f * Time.deltaTime;
     }
     void AnimationUpdate(bool b) {
